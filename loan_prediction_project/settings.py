b9425108a38a63000a12b640621a06d97f93ed25
Ryan Delgado <ryandelgado8@gmail.com>
2018-02-21 00:56:46 -0500

    Removed some other stuff. Added updates to the ML_Models subdir.

--- loan_prediction_project/settings.py
+++
@@ -1,6 +0,0 @@
-# settings.py
-# Initial settings script to set up the project. This module will be imported by all of the scripts
-# by Ryan Delgado 7/9/2016
-
-DATA_DIR = "raw_data"
-PROCESSED_DIR = "processed_data"
